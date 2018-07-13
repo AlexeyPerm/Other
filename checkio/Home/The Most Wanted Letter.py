888ad6551d0ba1c4d2634c1df4786b209a76fd7f
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-07-13 19:53:23 +0500

    промежуточное сохранение

---
+++ checkio/Home/The Most Wanted Letter.py
@@ -0,0 +1,32 @@
+def checkio(text: str) -> str:
+    import collections
+    result = []
+    for k in text:
+        if k.isalpha():
+            result.append(k.lower())
+    #b = collections.Counter(result)
+
+
+
+    return next(iter(collections.Counter(result)))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio("Hello34goi5jg54hg945asdasdasdas World!"))
+
+    # # These "asserts" using only for self-checking and not necessary for auto-testing
+    # assert checkio("Hello World!") == "l", "Hello test"
+    # assert checkio("How do you do?") == "o", "O is most wanted"
+    # assert checkio("One") == "e", "All letter only once."
+    # assert checkio("Oops!") == "o", "Don't forget about lower case."
+    # assert checkio("AAaooo!!!!") == "a", "Only letters."
+    # assert checkio("abe") == "a", "The First."
+    # print("Start the long test")
+    # assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    # print("The local tests are done.")
