966c45500f9ac3766be6bdcacc19cd4887874141
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-07-24 22:14:25 +0500

    промежуточное

---
+++ checkio/Home/sun-angle.py
@@ -0,0 +1,17 @@
+def sun_angle(time):
+    hour = int(time.split(':')[0])
+    min = int(time.split(':')[1])
+    if hour in range(6, 19):
+        return (hour - 6) * 15 + (min * 0.25)
+    else:
+        return "I don't see the sun!"
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sun_angle("07:00"))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert sun_angle("07:00") == 15
+    assert sun_angle("01:23") == "I don't see the sun!"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
