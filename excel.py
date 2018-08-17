1b4a9961f9a3712a995f2def585a6779029f2196
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-08-17 16:38:57 +0500

    заменил на result.txt

--- excel.py
+++ excel.py
@@ -8,7 +8,7 @@ source_xls = 'C:\\Users\\**\\Documents\\Миграция\\**.xlsx'
 vlans_list = xlrd.open_workbook(vlans_xls).sheet_by_index(0)
 source_list = xlrd.open_workbook(source_xls).sheet_by_index(0)
 
-with open('result', 'w') as dst_file:
+with open('result.txt', 'w') as dst_file:
     for ip_address in range(1, source_list.nrows):
         dst_file.write(f'\n{source_list.cell_value(ip_address, 4)}')
         for upper_vlan in range(1, vlans_list.nrows):
