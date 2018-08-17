6f7334e9c754c531763b053303524776cdb60289
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-08-17 16:24:41 +0500

    migration

--- excel.py
+++ excel.py
@@ -8,9 +8,9 @@ source_xls = 'C:\\Users\\**\\Documents\\Миграция\\**.xlsx'
 vlans_list = xlrd.open_workbook(vlans_xls).sheet_by_index(0)
 source_list = xlrd.open_workbook(source_xls).sheet_by_index(0)
 
-with open('text.txt', 'w') as dst_file:
+with open('result', 'w') as dst_file:
     for ip_address in range(1, source_list.nrows):
+        dst_file.write(f'\n{source_list.cell_value(ip_address, 4)}')
         for upper_vlan in range(1, vlans_list.nrows):
             if source_list.cell_value(ip_address, 4) == vlans_list.cell_value(upper_vlan, 0):
-                dst_file.write(f'{source_list.cell_value(ip_address, 4)}\t\t'
-                               f'{int(vlans_list.cell_value(upper_vlan, 1))}\n')
+                dst_file.write(f'\t{int(vlans_list.cell_value(upper_vlan, 1))}')
